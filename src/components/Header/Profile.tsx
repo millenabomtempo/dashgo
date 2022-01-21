5abaf0d2e3da6d78d9e9e2b62c84a87a7ab2ea00
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-01-21 15:45:06 -0300

    add responsivity to component header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,23 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile(){
+interface ProfileProps {
+  showProfileData?: boolean
+}
+
+export function Profile({ showProfileData= true }: ProfileProps){
   return(
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Millena Brito Bomtempo</Text>
-        <Text
-          color="gray.300"
-          fontSize="small"
-        >
-          millena.bomtempo@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Millena Brito Bomtempo</Text>
+          <Text
+            color="gray.300"
+            fontSize="small"
+          >
+            millena.bomtempo@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar size="md"  name="Millena Brito Bomtempo" src="https://github.com/millenabomtempo.png" />
     </Flex>
   )
