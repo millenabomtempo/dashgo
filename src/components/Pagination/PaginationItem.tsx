d4e44c5a7bb8cc4ace684b071d99d6a1eb898893
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-01-21 16:36:13 -0300

    add responsivity to component pagination

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -9,32 +9,32 @@ export function PaginationItem({ isCurrent = false, number }: PaginationItemProp
   if (isCurrent) {
     return(
       <Button
-          size="sm"
-          fontSize="xs"
-          width="4"
-          colorScheme="pink"
-          disabled
-          _disabled={{
-            bg: 'pink.500',
-            cursor: 'default'
-          }}
-        >
-          {number}
-        </Button>
+        size="sm"
+        fontSize="xs"
+        width="4"
+        colorScheme="pink"
+        disabled
+        _disabled={{
+          bg: 'pink.500',
+          cursor: 'default'
+        }}
+      >
+        {number}
+      </Button>
     )
   }
   
   return(
     <Button
-          size="sm"
-          fontSize="xs"
-          width="4"
-          bg="gray.700"
-          _hover={{
-            bg: 'gray.500',
-          }}
-        >
-          {number}
-        </Button>
+      size="sm"
+      fontSize="xs"
+      width="4"
+      bg="gray.700"
+      _hover={{
+        bg: 'gray.500',
+      }}
+    >
+      {number}
+    </Button>
   )
 }
