96fdda7484d26dd0aaf5c24361333b7e0c93f9e9
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-01-26 19:49:57 -0300

    forms and validations

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,9 +1,8 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
 import { RiSearchLine } from "react-icons/ri";
 
-
 export function SearchBox() {
-  return(
+    return(
     <Flex
       as="label"
       flex="1"
@@ -23,9 +22,7 @@ export function SearchBox() {
         ps="4"
         mr="4"
         placeholder="Buscar na plataforma"
-        _placeholder={{
-          color: 'gray.400'
-        }}
+        _placeholder={{ color: 'gray.400' }}
       />
       <Icon as={RiSearchLine} fontSize="20" />
     </Flex>
