a6c981de3254e038be151389298bffd58da03a95
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-01-31 10:55:23 -0300

    add miragejs, faker and react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,29 @@
 import { ChakraProvider } from "@chakra-ui/react"
 import { AppProps } from "next/app"
 import { theme } from "../styles/theme"
+import { makeServer } from "../services/mirage"
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 import { SidebarDrawerProvider } from "../components/context/SidebarDrawerContext"
+import { QueryClient, QueryClientProvider } from "react-query"
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer()
+}
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
         <SidebarDrawerProvider>
           <Component {...pageProps} />
         </SidebarDrawerProvider>
       </ChakraProvider>
-    )
+      <ReactQueryDevtools />
+    </QueryClientProvider> 
+  )
 }
 
 export default MyApp
